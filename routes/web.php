f295b21f2af2c36971b3995725960874ecefc303
Abir <abiruzzaman.molla@gmail.com>
2022-06-01 21:18:09 +0600

    fix: routes

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ include(base_path('routes/commands.php'));
 include(base_path('routes/frontend.php'));
 include(base_path('routes/admin.php'));
 include(base_path('routes/backend.php'));
+include(base_path('routes/user.php'));
 
 Route::any('/test', function(){
     dd(Auth::guard('web')->user());
