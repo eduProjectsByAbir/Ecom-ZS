8708e31bec5e3dc0abd62d1c6f487b172b6c09c3
Abir <abiruzzaman.molla@gmail.com>
2022-06-02 16:21:27 +0600

    feat: added prity routs and brands routes

--- routes/admin.php
+++ routes/admin.php
@@ -10,7 +10,9 @@ Route::group(['prefix' => 'admin', 'middleware' => ['admin:admin']], function(){
     Route::post('/login', [AdminController::class, 'store'])->name('admin.login');
 });
 
-Route::middleware(['is_admin', 'auth:sanctum,admin', config('jetstream.auth_session'), 'verified'])->prefix('admin/profile')->name('admin.profile.')->group(function () {
+Route::middleware(['is_admin', 'auth:sanctum,admin', config('jetstream.auth_session'), 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    // Admin Profile Routes
+    Route::prefix('profile')->name('profile.')->group(function () {
     Route::get('/', [AdminProfileController::class, 'show'])->name('show');
     Route::get('/edit', [AdminProfileController::class, 'edit'])->name('edit');
     Route::put('/update', [AdminProfileController::class, 'update'])->name('update');
@@ -18,4 +20,13 @@ Route::middleware(['is_admin', 'auth:sanctum,admin', config('jetstream.auth_sess
     Route::put('/update/password', [AdminProfileController::class, 'updatePassword'])->name('update.password');
     Route::delete('/delete', [AdminProfileController::class, 'distroy'])->name('delete');
     Route::get('admin/logout', [AdminController::class, 'destroy'])->name('logout');
+    });
+
+    Route::prefix('brand')->name('brand.')->group(function () {
+        Route::get('/', [AdminProfileController::class, 'index'])->name('index');
+        Route::put('/create', [AdminProfileController::class, 'store'])->name('store');
+        Route::get('/edit', [AdminProfileController::class, 'edit'])->name('edit');
+        Route::put('/update', [AdminProfileController::class, 'update'])->name('update');
+        Route::delete('/delete', [AdminProfileController::class, 'distroy'])->name('delete');
+    });
 });
