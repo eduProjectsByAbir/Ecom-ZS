770870becc697633ca846cec1a8737e86344dba2
Abir <abiruzzaman.molla@gmail.com>
2022-06-04 10:05:29 +0600

    feat: category database setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
         \App\Models\Brand::factory(15)->create();
+        \App\Models\Category::factory(15)->create();
         // \App\Models\Admin::factory()->create();
 
         $this->call([
