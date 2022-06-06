6101a5ac8e7b78c0165bd9932fd2364e3ed2e896
Abir <abiruzzaman.molla@gmail.com>
2022-06-06 17:42:51 +0600

    feat: show category in frontend

--- resources/views/frontend/layouts/partials/header.blade.php
+++ resources/views/frontend/layouts/partials/header.blade.php
@@ -77,15 +77,10 @@
                                     <li class="dropdown"> <a class="dropdown-toggle" data-toggle="dropdown"
                                             href="category.html">Categories <b class="caret"></b></a>
                                         <ul class="dropdown-menu" role="menu">
-                                            <li class="menu-header">Computer</li>
+                                            @foreach ($categories as $category)
                                             <li role="presentation"><a role="menuitem" tabindex="-1"
-                                                    href="category.html">- Clothing</a></li>
-                                            <li role="presentation"><a role="menuitem" tabindex="-1"
-                                                    href="category.html">- Electronics</a></li>
-                                            <li role="presentation"><a role="menuitem" tabindex="-1"
-                                                    href="category.html">- Shoes</a></li>
-                                            <li role="presentation"><a role="menuitem" tabindex="-1"
-                                                    href="category.html">- Watches</a></li>
+                                                href="">- {{ $category->name }}</a></li>
+                                            @endforeach
                                         </ul>
                                     </li>
                                 </ul>
