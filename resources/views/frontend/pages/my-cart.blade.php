2589546291895f3b27e4e4cf64c246cfb86888d0
Abir <abiruzzaman.molla@gmail.com>
2022-06-12 11:41:58 +0600

    fix: remove unused colum

--- resources/views/frontend/pages/my-cart.blade.php
+++ resources/views/frontend/pages/my-cart.blade.php
@@ -29,7 +29,6 @@ My Cart
                                     <th class="cart-romove item">Remove</th>
                                     <th class="cart-description item">Image</th>
                                     <th class="cart-product-name item">Product Name</th>
-                                    <th class="cart-edit item">Edit</th>
                                     <th class="cart-qty item">Quantity</th>
                                     <th class="cart-sub-total item">Subtotal</th>
                                     <th class="cart-total last-item">Grandtotal</th>
@@ -91,7 +90,6 @@ My Cart
                                         </div>
                                         @endif
                                     </td>
-                                    <td class="cart-product-edit"><a href="#" class="product-edit">Edit</a></td>
                                     <td class="cart-product-quantity">
                                         <div class="quant-input">
                                             <div class="arrows">
