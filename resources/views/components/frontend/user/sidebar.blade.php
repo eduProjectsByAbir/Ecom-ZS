e0faeb69407be2ab4bbc2b1d48cbdeebd30e9b5c
Abir <abiruzzaman.molla@gmail.com>
2022-06-15 19:26:14 +0600

    fix: fix

--- resources/views/components/frontend/user/sidebar.blade.php
+++ resources/views/components/frontend/user/sidebar.blade.php
@@ -1,7 +1,7 @@
 <img src="{{ asset(auth('web')->user()->profile_photo_url) }}" alt="" class="card-img-top" id="userImage">
 <ul class="list-group list-group-flush">
-    <a href="{{ route('user.home') }}" class="btn {{ Route::is('user.home') ? 'btn-success' : 'btn-info' }} btn-sm btn-block">Home</a>
-    <a href="{{ route('user.myorders') }}" class="btn {{ Route::is('user.myorders') ? 'btn-success' : 'btn-info' }} btn-sm btn-block">Orders</a>
+    <a href="{{ route('user.home') }}" class="btn {{ Route::is('user.home') || Route::is('user.dashboard') ? 'btn-success' : 'btn-info' }} btn-sm btn-block">Home</a>
+    <a href="{{ route('user.myorders') }}" class="btn {{ Route::is('user.myorders') || Route::is('user.myorder.details') ? 'btn-success' : 'btn-info' }} btn-sm btn-block">Orders</a>
     <a href="{{ route('user.profile') }}" class="btn {{ Route::is('user.profile') ? 'btn-success' : 'btn-info' }} btn-sm btn-block">Profile Update</a>
     <a href="{{ route('user.profile.password') }}" class="btn {{ Route::is('user.profile.password') ? 'btn-success' : 'btn-info' }} btn-sm btn-block">Change Password</a>
     <a href="{{ route('user.logout') }}" class="btn btn-danger btn-sm btn-block">Logout</a>
